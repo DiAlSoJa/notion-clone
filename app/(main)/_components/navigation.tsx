fdec5e038108fc45dad5e30869ad297cd4575530
DiAlSoJa <sotodiego37@gmail.com>
2024-07-13 02:59:55 -0600

    hora del curso 4:32

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -1,6 +1,6 @@
 "use client"
 import { cn } from "@/lib/utils";
-import { ChevronsLeft, MenuIcon, PlusCircle, Search, Settings } from "lucide-react";
+import { ChevronsLeft, MenuIcon, Plus, PlusCircle, Search, Settings, Trash } from "lucide-react";
 import { usePathname } from "next/navigation";
 import { useRef, ElementRef, useState, useEffect } from "react";
 import { useMediaQuery } from "usehooks-ts";
@@ -10,6 +10,8 @@ import { useMutation} from "convex/react";
 import { Item } from "./item";
 import { toast } from "sonner";
 import { DocumentList } from "./document-list";
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
+import {TrashBox} from "./trash-box";
 
 
 export const Navigation = () => {
@@ -22,7 +24,7 @@ export const Navigation = () => {
     const navbarRef = useRef<ElementRef<"div">>(null);
     const [isResetting, setIsResetting] = useState(false);
     const [isCollapsed, setIsCollapsed] = useState(isMobile);
-    // console.log(documents)
+
     useEffect(()=>{
         if(isMobile){
             collapse();
@@ -144,6 +146,22 @@ export const Navigation = () => {
             </div>
             <div className="mt-4">
                   <DocumentList/>
+                  <Item 
+                    onClick={handleCreate}
+                    icon={Plus}
+                    label="Add a page"
+                  />
+                  <Popover>
+                    <PopoverTrigger className="w-full mt-4">
+                      <Item label="Trash" icon={Trash} onClick={()=>{}} />
+                    </PopoverTrigger>
+                    <PopoverContent
+                      className="p-0 w-72"
+                      side={isMobile ? "bottom" : "right"}
+                    >
+                      <TrashBox/>
+                    </PopoverContent>
+                  </Popover>
             </div>
 
             <div
